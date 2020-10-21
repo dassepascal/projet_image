5c5e0fd8b9b3e352bf85d6a715d35eb4fef06c54
dassepascal <pascaldasse56@gmail.com>
2020-10-21 11:03:12 +0200

    init

--- process_image.php
+++ process_image.php
@@ -9,3 +9,12 @@ if (!isset($_POST['formImageSubmit'])) {
   $error_msg = 'Aucune donnée n\'est fournie.<a href="' . WEB_DIR_URL . 'admin.php">retour</a>';
   var_dump(WEB_DIR_URL . 'admin.php');
 }
+if (isset($_POST['formImageSubmit'])) {
+  $title        = $_POST['title'];
+  $descr        = $_POST['descr'];
+  $filename     = $_POST['filename'];
+  if ((empty($title)) || (empty($descr)) || (empty($filename))) {
+
+    $error_msg = ' une des informations est manquante.<a href="' . WEB_DIR_URL . 'admin.php">retour</a>';
+  }
+}
