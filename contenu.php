1004c36d8791b96dc52bf2762cb3b1d7d1d29f62
dassepascal <pascaldasse56@gmail.com>
2020-10-21 09:03:25 +0200

    remplace $image_dir_url par IMAGE_DIR_URL

--- contenu.php
+++ contenu.php
@@ -1,5 +1,6 @@
 <?php
 echo '<h4> contenu.php </h4>';
+require('config.php');
 require('class/Image.php');
 $image = new Image();
 // definition du chemin et de l(URL du repertoire image
@@ -7,8 +8,8 @@ $image = new Image();
 $image_dir_path = $_SERVER['DOCUMENT_ROOT'] . '/projet_imagebis/images/';
 var_dump($image_dir_path);
 
-$image_dir_url = 'http://' . $_SERVER['HTTP_HOST'] . '/projet_imagebis/images/';
-var_dump($image_dir_url);
+/*$image_dir_url = 'http://' . $_SERVER['HTTP_HOST'] . '/projet_imagebis/images/';
+var_dump($image_dir_url);*/
 
 // affectation dans la variable $images du resultat de la methode getImages
 $images = $image->getImages($image_dir_path);
@@ -16,5 +17,6 @@ var_dump($images);
 // affichage;
 ?>
 <?php foreach ($images as $image) : ?>
-  <li><img src="<?php echo $image_dir_url  . $image ?>" /></li>
+  <li><img src="<?php echo IMAGE_DIR_URL  . $image ?>" /></li>
+  <?php var_dump(IMAGE_DIR_URL  . $image); ?>
 <?php endforeach ?>
