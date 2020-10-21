6652b992a56df57a09b558cb8c00a8c8e400ce01
dassepascal <pascaldasse56@gmail.com>
2020-10-21 10:29:51 +0200

    init

--- admin.php
+++ admin.php
@@ -9,24 +9,29 @@ $image = new Image();
 
 $images = $image->getImages(IMAGE_DIR_PATH);
 ?>
-<h1><?php echo WEB_TITLE ?></h1>
+
+
 <?php $image_dir_url = 'http://' . $_SERVER['HTTP_HOST'] . '/projet_imagebis/images/';
 ?>
-<?php foreach ($images as $image) : ?>
-  <li><img src="<?php echo $image_dir_url  . $image ?>" /></li>
+<h1><?php echo WEB_TITLE ?></h1>
+<ul>
+  <?php foreach ($images as $image) : ?>
+
+    <li><img src="<?php echo $image_dir_url  . $image ?>" />
 
 
-  <form method="post" action="process_image.php">
-    <p> Titre :<input type="text" name="title" /></p>
-    <input type="hidden" name="filename" value="<?php echo $image ?>" />
-
-  </form>
-
-  <p>Description <br><textarea name="descr" cols="50" rows=5></textarea></p>
-
-
+      <form method="post" action="process_image.php">
+        <p> Titre :<input type="text" name="title" /></p>
+        <input type="hidden" name="filename" value="<?php echo $image ?>" />
 
 
 
+        <p>Description <br><textarea name="descr" cols="50" rows=5></textarea></p>
+        <p><input type="submit" name="formImageSubmit" value="validez" /></p>
 
-<?php endforeach ?>
+
+
+      </form>
+    </li>
+  <?php endforeach ?>
+</ul>
