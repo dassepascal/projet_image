a934a2d8334ac56b26b4c29b86e62f20b02b0e17
dassepascal <pascaldasse56@gmail.com>
2020-10-21 10:22:03 +0200

    init

--- admin.php
+++ admin.php
@@ -1,3 +1,24 @@
 <?php
+
 echo 'hello';
-require('contenu.php');
+require('config.php');
+require('class/Image.php');
+//require('contenu.php');
+
+$image = new Image();
+
+$images = $image->getImages(IMAGE_DIR_PATH);
+?>
+<h1><?php echo WEB_TITLE ?></h1>
+<?php $image_dir_url = 'http://' . $_SERVER['HTTP_HOST'] . '/projet_imagebis/images/';
+?>
+<?php foreach ($images as $image) : ?>
+  <li><img src="<?php echo $image_dir_url  . $image ?>" /></li>
+
+
+  <form method="post" action="process_image.php">
+    <p> Titre :<input type="text" name="title" /></p>
+    <input type="hidden" name="filename" value="<?php echo $image ?>" />
+
+  </form>
+<?php endforeach ?>
