89632b77f16393967346015d5054e6203482bdc3
dassepascal <pascaldasse56@gmail.com>
2020-10-22 18:10:26 +0200

    init const IMAGE_DIR_PATH

--- contenu.php
+++ contenu.php
@@ -5,14 +5,14 @@ require('class/Image.php');
 $image = new Image();
 // definition du chemin et de l(URL du repertoire image
 // chemin (path) du repertoire images
-$image_dir_path = $_SERVER['DOCUMENT_ROOT'] . '/projet_imagebis/images/';
+//$image_dir_path = $_SERVER['DOCUMENT_ROOT'] . '/projet_imagebis/images/';
 
 
 //$image_dir_url = 'http://' . $_SERVER['HTTP_HOST'] . '/projet_imagebis/images/';
 
 
 // affectation dans la variable $images du resultat de la methode getImages
-$images = $image->getImages($image_dir_path);
+$images = $image->getImages(IMAGE_DIR_PATH);
 
 // affichage;
 ?>
