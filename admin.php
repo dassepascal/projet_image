46bfde5b969d7474db39a644879f954bce027adc
dassepascal <pascaldasse56@gmail.com>
2020-10-28 22:07:45 +0100

    init essai

--- admin.php
+++ admin.php
@@ -14,27 +14,46 @@ $images = $image->getImages(IMAGE_DIR_PATH);
 
 
 <?php $image_dir_url = 'http://' . $_SERVER['HTTP_HOST'] . '/projet_imagebis/images/'; ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>admin</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
 
 
-<h1><?php echo WEB_TITLE ?></h1>
 
-<ul>
+  <h1><?php echo WEB_TITLE ?></h1>
 
-  <?php foreach ($images as $image) : ?>
+  <ul>
 
-    <li><img src="<?php echo $image_dir_url  . $image['filename'] ?>" />
-      <form method="post" action="process_image.php">
-        <?php if (!empty($image['title'])) : ?>
-          <input type="hidden" name="update" value="1" />
+    <?php foreach ($images as $image) : ?>
+      <ul class="menu">
+        <li><a href="admin.php">Administration</a></li>
+        <li><a href="upload.php"> Upload</a></li>
+        <li><a href="index.php">Site web</a></li>
+      </ul>
+      <li><img class="img" src="<?php echo $image_dir_url  . $image['filename'] ?>" />
+        <form method="post" action="process_image.php">
 
-        <?php endif ?>
-        <p> Titre :<input type="text" name="title" value="<?php echo $image['title']  ?>"></p>
+          <?php if (!empty($image['title'])) : ?>
+            <input type="hidden" name="update" value="1" />
 
-        <input type="hidden" name="filename" value="<?php echo $image['filename']; ?>" />
-        <p>Description <br><textarea name="descr" cols="50" rows=5><?php echo $image['description']; ?></textarea></p>
-        <p><input type="submit" name="formImageSubmit" value="validez" /></p>
-      </form>
-    </li>
-  <?php endforeach ?>
+          <?php endif ?>
+          <p> Titre :<input type="text" name="title" value="<?php echo $image['title']  ?>"></p>
 
-</ul>
+          <input type="hidden" name="filename" value="<?php echo $image['filename']; ?>" />
+          <p>Description <br><textarea name="descr" cols="50" rows=5><?php echo $image['description']; ?></textarea></p>
+          <p><input type="submit" name="formImageSubmit" value="validez" /></p>
+        </form>
+      </li>
+    <?php endforeach ?>
+
+  </ul>
+</body>
+
+</html>
