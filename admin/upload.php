34d28013e9ef89b18820f63a71345f9943bb8b96
dassepascal <pascaldasse56@gmail.com>
2020-10-30 11:13:55 +0100

    init admin

--- admin/upload.php
+++ admin/upload.php
@@ -1,3 +1,36 @@
+<?php
+echo 'upload';
+$upload_error = array(
+
+  UPLOAD_ERR_OK             => "No errors.",
+  UPLOAD_ERR_INI_SIZE       => "Larger than upload_max_filesize.",
+  UPLOAD_ERR_FORM_SIZE      => "Larger than form MAX_FILE_SIZE.",
+  UPLOAD_ERR_PARTIAL        => " Partial upload",
+  UPLOAD_ERR_NO_FILE        => "No file.",
+  UPLOAD_ERR_NO_TMP_DIR     => "No temporary directory.",
+  UPLOAD_ERR_CANT_WRITE     => "Can't write to disk.",
+  UPLOAD_ERR_EXTENSION       => "File upload stopped by extension."
+);
+var_dump('#1');
+
+if (isset($_POST['submit'])) {
+  var_dump('#2');
+  $tmp_file = $_FILES['file_upload']['tmp_name'];
+
+  var_dump($tmp_file);
+  $target_file = basename($_FILES['file_upload']['name']);
+  var_dump($target_file);
+  $upload_dir = 'c:\wamp64/www/projet_imagebis/images/';
+  var_dump($upload_dir);
+  if (move_uploaded_file($tmp_file, $upload_dir . "/" . $target_file)) {
+    $message = "File uploaded successfully.";
+  } else {
+    $error = $_FILES['file_upload']['error'];
+    $message = $upload_errors[$error];
+  }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -9,40 +42,14 @@
 
 <body>
   <div id="container">
-    <?php print_r($_FILES);
-    ?>
 
-    <form id="uploadForm" action="" method="post" enctype="multipart/form-data">
+    <form id="uploadForm" action="upload.php" method="post" enctype="multipart/form-data">
       <p>Ajouter des images</p>
-      <input type="file" value="" name="upload[]" multiple="multiple">
+      <input type="file" value="" name="file_upload" multiple="multiple">
 
-      <input id="uploadFormSubmit" name="uploadFormSubmit" type="submit">
+      <input id="uploadFormSubmit" name="submit" type="submit">
     </form>
-    <?php
-    $upload_dir = 'c:\wamp64/www/projet_imagebis/images/';
-    var_dump($upload_dir);
 
-
-
-    if (isset($_FILES['upload']['error']));
-    var_dump($_FILES['upload']['error']);
-    foreach ($_FILES['upload']['error'] as $key => $error) {
-
-
-      if ($error == UPLOAD_ERR_OK) //aucune erreur
-        var_dump($error);; {
-        $tmp_name = $_FILES['upload']['tmp_name'][$key];
-        var_dump($tmp_name);
-
-        $name = $_FILES['upload']['name'][$key];
-        var_dump($name);
-
-        move_uploaded_file($tmp_name, $upload_dir . $name);
-        exit(); //deplacement
-        var_dump(move_uploaded_file($tmp_name, $upload_dir . $name));
-      }
-    }
-    ?>
   </div>
 </body>
 
