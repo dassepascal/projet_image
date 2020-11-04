5a46544eabbb2a2321c31674fe406f47df3bdad4
dassepascal <pascaldasse56@gmail.com>
2020-11-04 12:58:05 +0100

    init

--- process/process_auth.php
+++ process/process_auth.php
@@ -4,13 +4,16 @@ require('../class/User.php');
 if (isset($_POST['submitLoginForm'])) {
   // post user
   $user_login = $_POST['login'];
-
+  var_dump($user_login);
   $user_pass = $_POST['password'];
+  var_dump($user_pass);
   //instanciation de la classe User
   $user = new user();
   // utilisation de la methode authUser de la classe User
   $authuser = $user->authUser($user_login, $user_pass);
   if ($authuser['count'] == 0) {
+    var_dump($authuser);
     $msg_error = 'le compte n\'est pas reconnu';
+    echo $msg_error;
   }
 }
