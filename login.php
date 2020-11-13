f90fe757e654f8f05e90fdef127714349b8804b2
dassepascal <pascaldasse56@gmail.com>
2020-11-13 12:25:29 +0100

    designe css de login

--- login.php
+++ login.php
@@ -1,13 +1,28 @@
-<div id="login">
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>login</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+
+  <div id="login">
 
 
 
 
-  <form action="process/process_auth.php" method="post">
-    <p> login:<br />
-      <input type="text" name="login" /></p>
-    <p>Mot de passe :<br />
-      <input type="password" name="password"></br></p>
-    <input type="submit" name="submitLoginForm">
-  </form>
-</div>
+    <form action="process/process_auth.php" method="post">
+      <p> login:<br />
+        <input type="text" name="login" /></p>
+      <p>Mot de passe :<br />
+        <input type="password" name="password"></br></p>
+      <input type="submit" name="submitLoginForm">
+    </form>
+  </div>
+</body>
+
+</html>
